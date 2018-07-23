2561676dd9d7a7ed5f73ced423823a983d5b8f20
Gianni Gianino <giannigdev@macbook.lan>
2018-07-23 20:14:43 +0200

    Add window without Storyboard, LoginController

---
+++ FirebaseChatApp/Controllers/LoginController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginController.swift
+//  FirebaseChatApp
+//
+//  Created by Gianni Gianino on 23/07/2018.
+//  Copyright © 2018 indie. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor(red: 129/255, green: 147/255, blue: 166/255, alpha: 1)
+    }
+
+}
---
+++ FirebaseChatApp/Controllers/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  FirebaseChatApp
+//
+//  Created by Gianni Gianino on 23/07/2018.
+//  Copyright © 2018 indie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    //Adding button to the top
+    navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+    
+  }
+  
+  
+  @objc func handleLogout()
+  {
+    //launch Login controller
+    let loginController = LoginController()
+    present(loginController, animated: true, completion: nil)
+    
+  }
+
+  
+
+}
+
